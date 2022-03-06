b5c6b6f3ee335a88f3dd270eb35c4f70f343525c
Geraldo Costa <geraldocoosta@gmail.com>
2022-03-06 12:35:43 -0300

    aprendendo while em bash scripts

--- for.sh
+++ for.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Repare que a lista no script é sepada por espaços, e não tem delimitador
-for var in pastel pizza esfiha 'pão de queijo' 
+for var in pastel pizza esfiha 'pão de queijo'
 do
   echo Adoro $var
 done
